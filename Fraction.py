5253241ef65c5e8ddd2cc09e4b2ccc328064a54e
xxing21 <xxing21@cmc.edu>
2020-01-30 16:20:56 -0800

    fix the bug

--- Fraction.py
+++ Fraction.py
@@ -36,9 +36,7 @@ class Fraction:
         return firstnum == secondnum
 
     def __mul__(self, other):
-        pass
-
-        #num = self.num * other.num
-        #den = self.den * other.den
-        #common = gcd(num,den)
-        #return Fraction(num//common,den//common)
+        num = self.num * other.num
+        den = self.den * other.den
+        common = gcd(num,den)
+        return Fraction(num//common,den//common)
